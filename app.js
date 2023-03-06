41d1892b36a9bdd37a9476c0ca58e188010c91f8
Ollatomiwa <109435886+Ollatomiwa@users.noreply.github.com>
2023-03-06 23:27:38 +0100

    first data

---
+++ app.js
@@ -0,0 +1,8 @@
+var data = require ('./data')
+
+results = data.printUserDetails(names = "Ollatomiwa", age = 30, MatricID = "ADF567HG"), score = 56;
+results1 = data.printUserDetails1(names1 = "Tommy", age1 = 23, MatricID1 = "BV567HG"), score1 = 34;
+
+
+console.log(`Name: ${names}, Age: ${age}, Matric-ID: ${MatricID}, C.A Score: ${score}`);
+console.log(`Name: ${names1}, Age: ${age1}, Matric-ID: ${MatricID1}, C.A Score: ${score1} `);
---
+++ data.js
@@ -0,0 +1,11 @@
+exports.printUserDetails = function printUserDetails(names, age, MatricID, scores) {
+    return names,age, MatricID, scores;
+}
+
+exports.printUserDetails1 = function printUserDetails1(names1, age1, MatricID1, scores1) {
+    return names1,age1, MatricID1, scores1;
+}
+
+
+
+
